d447da6ebe7975dda659bde74f492784ac3a840a
riverbook708 <riverbook708@gmail.com>
2022-03-15 23:33:59 +0900

    section 4

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -8,21 +8,21 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    public function index(int $id) {
+  public function index(int $id) {
 
-      // 全てのフォルダを取得する
-      $folders = Folder::all();
+    // 全てのフォルダを取得する
+    $folders = Folder::all();
 
-      // 選ばれたフォルダを取得する
-      $current_folder = Folder::find($id);
+    // 選ばれたフォルダを取得する
+    $current_folder = Folder::find($id);
 
-      // 選ばれたフォルダに紐づくタスクを取得する
-      $tasks = Task::where('folder_id', $current_folder->id)->get();
+    // 選ばれたフォルダに紐づくタスクを取得する
+    $tasks = $current_folder->tasks()->get();
 
-      return view('tasks/index', [
-        'folders' => $folders,
-        'current_folder_id' => $current_folder->id,
-        'tasks' => $tasks,
-      ]);
+    return view('tasks/index', [
+      'folders' => $folders,
+      'current_folder_id' => $current_folder->id,
+      'tasks' => $tasks,
+    ]);
     }
 }
